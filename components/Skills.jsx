19c2183efbbb4442998dd7f68e3194f7fc5bf8dd
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2023-01-24 11:48:28 -0300

    errors fixed

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import reactIMG from '/../public/assets/skills/html.png"';
+import reactIMG from '/../public/assets/skills/html.png';
 
 const Skills = () => {
   return (
